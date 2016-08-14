1ee614d08d90bbd2717c8f86542ac6af7d149c47
hanxlinsist <xinglongsuccess@outlook.com>
2016-08-14 20:13:54 +0800

    update

--- douban/douban/spiders/MovieSpider.py
+++ douban/douban/spiders/MovieSpider.py
@@ -11,26 +11,13 @@ class DoubanSpider(CrawlSpider):
     name = "douban"
     allowed_domains = ['douban.com']
     start_urls = ['https://book.douban.com/tag/']
-    #start_urls = ['https://book.douban.com/tag/%E4%BA%92%E8%81%94%E7%BD%91']
     
     # A dictionary of settings that will be overridden from the project wide configuration when running this spider. 
     #It must be defined as a class attribute since the settings are updated before instantiation.
     custom_settings = {
-    	"DEPTH_LIMIT" : 3,
-    	"DOWNLOAD_TIMEOUT" : 190
-    	}
-
-    #def start_requests(self):
-    #    headers = {
-    #        "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    #        "Accept-Encoding":"gzip, deflate, sdch, br",
-    #       "Accept-Language":"en-US,en;q=0.8",
-    #        "Cache-Control":"max-age=0",
-    #        "Connection":"keep-alive",
-    #        "Host":"www.douban.com",
-    #        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36"
-    #    }
-    #    yield scrapy.Request('https://www.douban.com/')
+        "DEPTH_LIMIT" : 3,
+        "DOWNLOAD_TIMEOUT" : 190
+        }
 
 
     rules = [
@@ -61,5 +48,5 @@ class DoubanSpider(CrawlSpider):
         item['twostar'] = e.xpath('//*[@id="interest_sectl"]/descendant::span[@class = "rating_per"][4]/text()').extract()
         item['onestar'] = e.xpath('//*[@id="interest_sectl"]/descendant::span[@class = "rating_per"][5]/text()').extract()
 
-	return item
+        return item
 
