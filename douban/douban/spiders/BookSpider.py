99bf4960213f38eb6e8d649ffedcc087521549a2
hanxlinsist <xinglongsuccess@outlook.com>
2016-08-14 16:58:30 +0800

    add pipeline attribute

--- douban/douban/spiders/BookSpider.py
+++ douban/douban/spiders/BookSpider.py
@@ -6,15 +6,22 @@ import numpy as np
 import scrapy
 from scrapy.selector import Selector
 from douban.items import BookItem
+import douban.pipelines as pipe
 
 class BookSpider(scrapy.Spider):
     name = "book.douban"
     allowed_domains = ['douban.com']
     start_urls = ['https://book.douban.com/tag/']
+    
+    # Item只被这个集合中的pipeline处理
+    pipeline = set([
+        pipe.IDPipeline,
+        pipe.BookInfoPipeline,
+    ])
 
     # 抽取标签页面的所有Tag链接，生成对应的请求
     def parse(self, response):
-	links = response.xpath("//*[@class = 'tagCol']/descendant::a/@href").extract()
+        links = response.xpath("//*[@class = 'tagCol']/descendant::a/@href").extract()
         for href in links:
             for pageNum in np.linspace(0, 180, 10): # 抓取每个Tag的前10页书籍
                 full_url = response.urljoin(href + "/?start=" + str(int(pageNum)) + "&type=S") # ?type=S  目的是按评价排序
@@ -22,7 +29,7 @@ class BookSpider(scrapy.Spider):
 
     # 抽取对应标签页面的所有书籍链接
     def parse_tag_per_page(self, response):
-	links = response.xpath("//ul[@class = 'subject-list']/descendant::a[@class = 'nbg']/@href").extract()
+        links = response.xpath("//ul[@class = 'subject-list']/descendant::a[@class = 'nbg']/@href").extract()
         for book in links:
             yield scrapy.Request(book, callback=self.parse_book)
 
@@ -43,12 +50,12 @@ class BookSpider(scrapy.Spider):
         item['twostar'] = e.xpath('//*[@id="interest_sectl"]/descendant::span[@class = "rating_per"][4]/text()').extract()
         item['onestar'] = e.xpath('//*[@id="interest_sectl"]/descendant::span[@class = "rating_per"][5]/text()').extract()
 
-	item['tag'] = response.xpath("//*[@id = 'db-tags-section']/descendant::a/text()").extract()
+        item['tag'] = response.xpath("//*[@id = 'db-tags-section']/descendant::a/text()").extract()
 
-	request = scrapy.Request(response.url + "/comments/hot", callback=self.parse_review) # 发送书籍对应的短评请求
-	request.meta['item'] = item
+        request = scrapy.Request(response.url + "/comments/hot", callback=self.parse_review) # 发送书籍对应的短评请求
+        request.meta['item'] = item
 
-	return request
+        return request
 
 
     # 抽取书籍的热门短评信息
